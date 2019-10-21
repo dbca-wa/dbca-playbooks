6229c4f3863617198dd614febdaae729118d691c
Scott Percival <scott.percival@dbca.wa.gov.au>
2019-10-21 14:00:17 +0800

    roles.nginx: fix buffering

--- edit_nginx.sh
+++ edit_nginx.sh
@@ -5,6 +5,7 @@ set -e
 
 # Edit the pillar file
 vim /etc/ansible/group_vars/nginx/nginx.yml
+pushd /etc/ansible; git commit -am 'updated nginx rules'; popd
 
 # Update the Let's Encrypt certificates
 ./roles/nginx/scripts/cert-scrub.py
