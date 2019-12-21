e1f8648a9d51e54aaa031438dafa7cac879399ac
DBCA ASI <asi@dpaw.wa.gov.au>
2019-12-21 21:04:11 +0800

    prepping for local docker build of nginx setup

---
+++ edit_nginx_local.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+. /etc/profile
+set -x
+set -e
+
+# Edit the pillar file
+vim /etc/ansible/group_vars/nginx/nginx.yml
+pushd /etc/ansible; git commit -am 'updated nginx rules'; popd
+
+# Update the Let's Encrypt certificates
+./roles/nginx/scripts/cert-scrub.py
+
+# run the nginx playbook
+ansible-playbook nginxlocal.yml
+
